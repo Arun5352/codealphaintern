03552dbd352f9c39e11fb78666d7443a40bb656d
Arun5352 <141246789+Arun5352@users.noreply.github.com>
2023-09-05 20:02:08 +0530

    Add files via upload

---
+++ Temperature Converter/script.js
@@ -0,0 +1,12 @@
+document.getElementById("convert").addEventListener("click", function () {
+    const celsiusInput = document.getElementById("celsius");
+    const resultElement = document.getElementById("result");
+
+    const celsius = parseFloat(celsiusInput.value);
+    if (!isNaN(celsius)) {
+        const fahrenheit = (celsius * 9/5) + 32;
+        resultElement.textContent = `Temperature in Fahrenheit: ${fahrenheit.toFixed(2)}°F`;
+    } else {
+        resultElement.textContent = "Please enter a valid temperature in Celsius.";
+    }
+});
